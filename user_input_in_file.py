0729564b6075bc7dc893ee7308e6db013af1ca30
Bhavuk Sagar <sahilsagar5@icloud.com>
2019-04-02 11:53:47 +0530

    Update user_input_in_file.py

--- user_input_in_file.py
+++ user_input_in_file.py
@@ -1,20 +1,39 @@
 class file:
     x=0
     def creation():                   #function for creating the file
-      print("Enter the file name with extention:")
-      global x
-      x=input()
-      f=open( x,'w')
-      print("Enter the data which you want to add in a file:")
-      data=input()
-      f.write(data)
-  
+        try:  
+            print("Enter the file name with extention:")
+            global x
+            x=input()
+            f=open( x,'w')
+            print("Enter the data which you want to add in a file:")
+            data=input()
+            f.write(data)
+            f.close()
+        except Exception:
+            print "Error"
   
     def reading():                    #function for reading the data from the file
-        global x
-        f=open(x,'r')
-        print(f.read())
-    
+        try:
+            global x
+            f=open(x,'r')
+            print(f.read())
+            f.close()
+        except Exception:
+            print "ERROR"
+    def app():                        #Function for append the new data in existing file 
+        try:
+            global x
+            f=open(x,'a')
+            print "Enter the new data:"
+            new_data=input()
+            f.write(new_data)
+            f.close()
+        except Exception:
+            print "Error"
+            
+
 obj=file
 obj.creation()
 obj.reading()
+obj.app()
