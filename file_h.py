49126639927fb7294fe38543ba35aef398500db7
Bhavuk Sagar <sahilsagar5@icloud.com>
2019-07-27 16:18:20 +0530

    Update file_h.py

--- file_h.py
+++ file_h.py
@@ -1,5 +1,4 @@
-class file:
-  
+class file: 
   def creation():                   #function for creating the file  
     f=open("abc.txt",'w')
     f.write("hello it is a text file")
@@ -8,7 +7,8 @@ class file:
   def reading():                    #function for reading the data from the file
     f=open("abc.txt",'r')
     print(f.read())
-    
+  
+#creating object of class and calling the function
 obj=file
 obj.creation()
 obj.reading()
